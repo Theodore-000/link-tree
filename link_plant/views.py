dca986f040f7da1ac740871c733c9faf5d33beca
Theodore-000 <Teddytez000@gmail.com>
2025-05-06 15:50:13 +0300

    views implemented

--- link_plant/views.py
+++ link_plant/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
