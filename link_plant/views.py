652910ab53021413fae4ac06faf6fd350415f135
Theodore-000 <Teddytez000@gmail.com>
2025-05-06 15:42:28 +0300

    view implemented

--- link_plant/views.py
+++ link_plant/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Profile, Link
 
 # Create your views here.
@@ -13,5 +13,22 @@ class LinkListView(ListView):
 class LinkCreateView(CreateView):
     model = Link
     fields = "__all__"
-    success_url = reverse_lazy('link-list')
-    
+    success_url = reverse_lazy("link-list")
+
+
+class LinkUpdateView(UpdateView):
+    model = Link
+    fields = ["text", "url"]
+    success_url = reverse_lazy("link-list")
+
+
+class LinkDeleteView(DeleteView):
+    model = Link
+    success_url = reverse_lazy("link-list")
+
+
+def profile_view(request, profile_slug):
+    profile = get_object_or_404(Profile, slug=profile_slug)
+    links = profile.link.all()
+    context = {"profile": profile, "links": links}
+    return render(request, "link_plant/profile.html", context)
