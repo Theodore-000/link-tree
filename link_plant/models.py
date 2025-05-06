7b987268439be7ee3b06067d958fb2e4b3a7c062
Theodore-000 <Teddytez000@gmail.com>
2025-05-06 13:51:50 +0300

    column changed

--- link_plant/models.py
+++ link_plant/models.py
@@ -1,26 +1,30 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
 # Create your models here.
 
 
 class Profile(models.Model):
-    
+
     BG_CHOICES = (
-        ('blue', 'Blue'),
-        ('green', 'Green'),
-        ('yellow', 'Yellow'),
-        
+        ("blue", "Blue"),
+        ("green", "Green"),
+        ("yellow", "Yellow"),
     )
     name = models.CharField(_("Name"), max_length=100)
     slug = models.SlugField(_("Slug"), max_length=100)
-    bg_color = models.CharField(_("Background Color"), max_length=50, choices=BG_CHOICES)
-    
-    
+    bg_color = models.CharField(
+        _("Background Color"), max_length=50, choices=BG_CHOICES
+    )
+
     def __str__(self):
         return self.name
-    
+
 
 class Link(models.Model):
-    text = models.TextField(_("Text"), max_length=100)
+    text = models.CharField(_("Text"), max_length=100)
     url = models.URLField(_("URL"), max_length=200)
     profile = models.ForeignKey(Profile, related_name="link", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
