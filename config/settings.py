0152a2b170dd5a7f5d61941af10845018f6efb6c
Theodore-000 <Teddytez000@gmail.com>
2025-05-06 14:31:30 +0300

    templates created

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Apps
     "link_plant",
+    # Third Party
+    "crispy_forms",
+    "crispy_tailwind",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+CRISPY_TEMPLATE_PACK = "tailwind"
--- link_plant/views.py
+++ link_plant/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
+from .models import Profile, Link
 
 # Create your views here.
+
+
+class LinkListView(ListView):
+    model = Link
+
+
+class LinkCreateView(CreateView):
+    model = Link
+    fields = "__all__"
+    success_url = reverse_lazy('link-list')
+    
